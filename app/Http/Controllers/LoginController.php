4af1396a50a2c518d7ea08ea50f0995bef10e18b
mycodesgit <lowexian@gmail.com>
2024-08-21 13:56:27 +0800

    Update

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -26,8 +26,8 @@ class LoginController extends Controller
         if ($validated) {
             $schlyear = '2024-2025';
             $semester = '1';
-            $stud_id = Auth::guard('students')->user()->id;
-            $stud = Students::where('schlyear', $schlyear)->where('semester', $semester)->find($stud_id);
+            $stud_id = Auth::guard('students')->user()->stud_id;
+            $stud = Students::where('schlyear', $schlyear)->where('semester', $semester)->first($stud_id);
 
             if ($stud->vc_id == null || $stud->vc_id == "no vc") {
                 $voucher = Voucher::where('status', 1)->inRandomOrder()->first();
