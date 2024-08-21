99ceaa14367be80dedb665d3e1bdc4b591de846c
mycodesgit <lowexian@gmail.com>
2024-08-21 22:15:28 +0800

    Update

--- resources/views/students/partials/numpad_control.blade.php
+++ resources/views/students/partials/numpad_control.blade.php
@@ -11,6 +11,79 @@
         <a id="btn-3" class="btn btn-app2" style="background-color: #6c757d; color: #000;">
             <i class="fas fa-3"></i>
         </a>
+        <a id="btn-4" class="btn btn-app2" style="background-color: #6c757d; color: #000;">
+            <i class="fas fa-4"></i>
+        </a>
+        <a id="btn-5" class="btn btn-app2" style="background-color: #6c757d; color: #000;">
+            <i class="fas fa-5"></i>
+        </a>
+    </div>
+    <div class="col-md-12">
+        <a id="btn-6" class="btn btn-app2" style="background-color: #6c757d; color: #000;">
+            <i class="fas fa-6"></i>
+        </a>
+        <a id="btn-7" class="btn btn-app2" style="background-color: #6c757d; color: #000;">
+            <i class="fas fa-7"></i>
+        </a>
+
+        <a id="btn-8" class="btn btn-app2" style="background-color: #6c757d; color: #000;">
+            <i class="fas fa-8"></i>
+        </a>
+
+        <a id="btn-9" class="btn btn-app2" style="background-color: #6c757d; color: #000;">
+            <i class="fas fa-9"></i>
+        </a>
+        <a id="btn-0" class="btn btn-app2" style="background-color: #6c757d; color: #000;">
+            <i class="fas fa-0"></i>
+        </a>
+    </div>
+    <div class="col-md-12">
+        <a id="btn-minus" class="btn btn-app2" style="background-color: #6c757d; color: #000;">
+            <i class="fas fa-minus"></i>
+        </a>
+
+        <a id="btn-reset" class="btn btn-app2" style="background-color: #fff782; color: #000;">
+            <i class="fas fa-arrow-left"></i>
+        </a>
+        <a id="btn-k" class="btn btn-app2" style="background-color: #fdff00 !important; color: #276e47;">
+            <i class="fas fa-k"></i>
+        </a>
+
+        <a id="btn-g" class="btn btn-app2" style="background-color: #fdff00 !important; color: #276e47;">
+            <i class="fas fa-g"></i>
+        </a>
+
+        <a id="btn-u" class="btn btn-app2" style="background-color: #fdff00 !important; color: #276e47;">
+            <i class="fas fa-u"></i>
+        </a>
+    </div>
+    <div class="col-md-12">
+        
+    </div>
+</div>
+@if(\Route::currentRouteName() == 'register')
+<hr>
+<div class="ml-1 mr-2 mt-2 mt-12">
+    <div class="col-md-12">
+        <a href="{{ route('studlogin') }}" class="btn btn-primary btn-block" style="color: #fff;">
+            <i class="fas fa-sign-in"></i> <b>Sign In</b>
+        </a>
+    </div>
+</div>
+@endif
+{{-- <div class="mt-4" style="text-align: center;">
+    <div class="col-md-12">
+        <a id="btn-1" class="btn btn-app2" style="background-color: #6c757d; color: #000;">
+            <i class="fas fa-1"></i>
+        </a>
+
+        <a id="btn-2" class="btn btn-app2" style="background-color: #6c757d; color: #000;">
+            <i class="fas fa-2"></i>
+        </a>
+
+        <a id="btn-3" class="btn btn-app2" style="background-color: #6c757d; color: #000;">
+            <i class="fas fa-3"></i>
+        </a>
     </div>
     <div class="col-md-12">
         <a id="btn-4" class="btn btn-app2" style="background-color: #6c757d; color: #000;">
@@ -74,4 +147,4 @@
         </a>
     </div>
 </div>
-@endif
+@endif --}}
